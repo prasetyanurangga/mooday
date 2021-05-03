a8e8e094397b37bf305d0328533b828b8b65a509
Angga Nur Prasetya <angganurprasetya5@gmail.com>
2021-05-03 20:50:10 +0700

    Make Mobile Version

---
+++ lib/bloc/mooday/track_bloc.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:mooday/bloc/mooday/track_event.dart';
+import 'package:mooday/bloc/mooday/track_state.dart';
+import 'package:mooday/models/track_model.dart';
+import 'package:mooday/provider/response_data.dart';
+import 'package:mooday/repository/mooday_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+class TrackBloc extends Bloc<TrackEvent, TrackState> {
+  MoodayRepository moodayRepository = MoodayRepository();
+
+  TrackBloc() : super(TrackInitial());
+
+  @override
+  TrackState get initialState =>TrackInitial();
+
+  @override
+  Stream<TrackState> mapEventToState(TrackEvent event) async* {
+    if (event is GetTrackByMood) { 
+      yield TrackLoading();
+      try {
+        final ResponseData<dynamic> response = await moodayRepository.getTracks(event.data);
+        print(response.);
+        print(event.data);
+        if (response.status == Status.ConnectivityError) {
+          yield const TrackFailure(error: "");
+        }
+        if (response.status == Status.Success) {
+          yield TrackSuccess(trackModel: response.data as List<TrackModel>);
+        } else {
+          yield TrackFailure(error: response.message);
+        }
+      } catch (error) {
+        // print(error);
+        yield TrackFailure(error: error.toString());
+      }
+    }
+  }
+}
---
+++ lib/component/raised_gradient_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:mooday/constant.dart';
+
+class RaisedGradientButton extends StatelessWidget {
+  final Widget child;
+  final Gradient gradient;
+  final double width;
+  final double height;
+  final Function onPressed;
+
+  const RaisedGradientButton({
+    Key key,
+    @required this.child,
+    this.gradient,
+    this.width = double.infinity,
+    this.height = 50.0,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      // margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: gradient, 
+        boxShadow: boxShadow,
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+            onTap: onPressed,
+            child: Center(
+              child: child,
+            )),
+      ),
+    );
+  }
+}
